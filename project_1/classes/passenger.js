8de3b8d80b2020602264dee5e4bdda85f25d5617
Yuval Zarfati <yuvalz19500@gmail.com>
2024-11-24 22:00:16 +0200

    add Booking and Passenger classes to manage flight bookings and passenger details

---
+++ project_1/classes/passenger.js
@@ -0,0 +1,8 @@
+class Passenger {
+    constructor(name, passportNumber) {
+        this.name = name; // Passenger's name
+        this.passportNumber = passportNumber; // Passport number
+    }
+}
+
+export default Passenger;
