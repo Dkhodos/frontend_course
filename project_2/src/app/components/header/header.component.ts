41b29bda1f9a8ad40dd7e44d18d053888cb5ea7d
Yuvalz19500 <yuvalz19500@gmail.com>
2024-12-08 23:04:47 +0200

    add header and footer components with basic structure and styles

---
+++ project_2/src/app/components/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ono-flight-header',
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+}
