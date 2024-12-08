38d60a3960e3ed31507cf4cc7d5bc132f71d639d
Yuvalz19500 <yuvalz19500@gmail.com>
2024-12-08 23:42:40 +0200

    add Tailwind CSS configuration and update header/footer components with Angular Material

--- project_2/src/app/components/footer/footer.component.ts
+++ project_2/src/app/components/footer/footer.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'ono-flight-footer',
-  imports: [],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
-export class FooterComponent {
-
-}
+export class FooterComponent {}
--- project_2/src/app/components/header/header.component.ts
+++ project_2/src/app/components/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'ono-flight-header',
-  imports: [],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
