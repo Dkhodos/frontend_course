e492214c3e16578e8576d18cf1e0dbcb3e321e68
Danny Khodos <dkhodos94@gmail.com>
2024-12-09 19:55:13 +0200

    prettier

--- project_2/src/app/pages/help/help.component.ts
+++ project_2/src/app/pages/help/help.component.ts
@@ -5,5 +5,6 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './help.component.html',
   styleUrl: './help.component.scss',
+  standalone: true
 })
 export class HelpComponent {}
