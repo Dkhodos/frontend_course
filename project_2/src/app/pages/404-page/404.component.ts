2c8a0c5098ee69275deb639c77c14afcbe2b903d
Danny Khodos <dkhodos94@gmail.com>
2024-12-15 19:05:16 +0200

    404 page, filter flights page

--- project_2/src/app/pages/404-page/404.component.ts
+++ project_2/src/app/pages/404-page/404.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {NotFoundPlaceholderComponent} from '../../components/not-found-placeholder/not-found-placeholder.component';
 
 @Component({
   selector: 'app-404',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NotFoundPlaceholderComponent],
   templateUrl: './404.component.html',
   styleUrls: ['./404.component.scss'],
 })
