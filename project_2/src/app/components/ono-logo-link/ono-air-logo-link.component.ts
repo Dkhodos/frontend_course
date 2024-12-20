cbb82bc75b5b207218b1924cbd65764f4758a036
Danny Khodos <dkhodos94@gmail.com>
2024-12-20 10:12:25 +0200

    @angular/common should mostly import CommonModule

--- project_2/src/app/components/ono-logo-link/ono-air-logo-link.component.ts
+++ project_2/src/app/components/ono-logo-link/ono-air-logo-link.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'ono-logo-link',
   templateUrl: './ono-logo-link.component.html',
   styleUrls: ['./ono-logo-link.component.scss'],
-  imports: [RouterLink, CommonModule],
+  imports: [RouterLink, NgOptimizedImage],
   standalone: true,
 })
 export class OnoLogoLinkComponent {}
