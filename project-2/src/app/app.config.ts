2c9e61d18f9830eaf7ee471b003f9aac09ba9468
Yuvalz19500 <yuvalz19500@gmail.com>
2025-01-12 18:03:58 +0200

    added firebase

--- project-2/src/app/app.config.ts
+++ project-2/src/app/app.config.ts
@@ -4,6 +4,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideNativeDateAdapter } from '@angular/material/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,5 +13,16 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideAnimationsAsync(),
     provideNativeDateAdapter(),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'ono-flight-f23eb',
+        appId: '1:680817298452:web:2655ea8f292dfbb1482b01',
+        storageBucket: 'ono-flight-f23eb.firebasestorage.app',
+        apiKey: 'AIzaSyAs9CgdsdwRIR4GSid9yLSLLudSO7vhq3g',
+        authDomain: 'ono-flight-f23eb.firebaseapp.com',
+        messagingSenderId: '680817298452',
+      })
+    ),
+    provideFirestore(() => getFirestore()),
   ],
 };
